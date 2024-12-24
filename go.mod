88f6dd30ea3c7bcd92cae2a63021578d05830d80
Kyle Brennan <kylos101@gmail.com>
2024-12-24 16:44:09 -0500

    Initial implementation

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.4
 replace github.com/kylos101/prusa-connect/v2/pkg/openapi => ./pkg/openapi
 
 require github.com/kylos101/prusa-connect/v2/pkg/openapi v0.0.0-00010101000000-000000000000
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/kylos101/prusa-connect/v2/pkg/openapi"
 )
@@ -30,6 +32,68 @@ func main() {
 		"basePath": baseURL.Path,
 		"scheme":   baseURL.Scheme,
 	})
+	cancelCtx, cancel := context.WithCancel(ctx)
+
+	token := os.Getenv("CONNECT_TOKEN")
+	if token == "" {
+		log.Fatal("No token provided")
+	}
+
+	connectInterval := os.Getenv("CONNECT_INTERVAL")
+	defaultInterval := 5 * time.Minute
+
+	interval, err := time.ParseDuration(connectInterval)
+	if err != nil {
+		log.Printf("Error parsing interval: %v, using default of 5m", err)
+		interval = defaultInterval
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	defer cancel()
+	for range ticker.C {
+		UploadSnapshot(cancelCtx, client, token)
+	}
+}
+
+func UploadSnapshot(ctx context.Context, client *openapi.APIClient, token string) {
+	// capture a still
+	stillFilename := "output.jpg"
+	cmd := exec.Command("rpicam-still", "-n", "-o", stillFilename)
+	defer func() {
+		log.Printf("Removing output.jpg")
+		if err := os.Remove("output.jpg"); err != nil {
+			log.Printf("Error removing output.jpg: %v", err)
+		}
+	}()
+
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Error running rpicam-still: %v", err)
+	}
+	log.Printf("rpicam-still output: %s/n", out)
+
+	// build a request to upload the still
+	stillFile, err := os.Open(stillFilename)
+	if err != nil {
+		log.Fatalf("Error opening still file: %v", err)
+	}
+	snapshotRequest := client.CameraAPI.CSnapshotPut(ctx)
+	snapshotRequest.Body(stillFile)
+	client.GetConfig().AddDefaultHeader("Token", token)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.GetConfig().AddDefaultHeader("Fingerprint", hostname)
+	// upload
+	response, err := client.CameraAPI.CSnapshotPutExecute(snapshotRequest)
+	if err != nil {
+		log.Fatalf("Error uploading snapshot: %v", err)
+	}
+	if response.StatusCode != 200 {
+		log.Printf("Unsuccessful snapshot upload: %v", response)
+	}
 
 }
 
